b724717cb79e5ba859f483a4ef6f5887c95f77f5
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-08-06 16:02:46 -0400

    Expression variable working

---
+++ js/script.js
@@ -0,0 +1,79 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+
+  "use strict";
+  
+  /* Variable declarations
+  ======================================== */
+  
+  var resultsBox = $("#results"),
+    buttonDict = {0: $('#zero'),
+                  1: $('#one'),
+                  2: $('#two'),
+                  3: $('#three'),
+                  4: $('#four'),
+                  5: $('#five'),
+                  6: $('#six'),
+                  7: $('#seven'),
+                  8: $('#eight'),
+                  9: $('#nine'),
+                  '.': $('#dot')},
+    keyCodeDict = {},
+    expression = '',
+    decimalInExp = false,
+    i,
+    key;
+  
+  for (i = 0; i <= 9; i += 1) {
+    keyCodeDict[i + 48] = i; // keyCodes for qwerty side of keyboard
+  }
+  keyCodeDict[46] = '.';
+  
+  /* Function declarations
+  ======================================== */
+
+  function updateResults(updateHTML) {
+    resultsBox.html(updateHTML);
+  }
+  
+  function handlerFactory(key) {
+    // function factory branching pattern
+    if (!isNaN(key)) { // check if key is a numeric string, e.g. isNaN('1') returns false, isNaN('.') returns true
+      return function (event) {
+        expression += event.data.value;
+        updateResults(expression);
+      };
+    } else if (key === '.') {
+      return function (event) {
+        if (!decimalInExp) { // allow only one decimal point per expression
+          decimalInExp = true;
+          expression += event.data.value;
+          updateResults(expression);
+        }
+      };
+    }
+  }
+  
+  /* Click Event Listener Attachments
+  ======================================== */
+  
+  for (key in buttonDict) {
+    if (buttonDict.hasOwnProperty(key)) {
+      // .click([eventData], handler) pattern to allow for frozen 'key'
+      buttonDict[key].click({ value: key }, handlerFactory(key)); // handlerFactory applied to return a handler function according to the key
+    }
+  }
+  
+  /* Key Press Events
+  ======================================== */
+  
+  $(document).keypress(function (event) {
+    var keyCode = event.keyCode;
+    if (keyCodeDict.hasOwnProperty(keyCode)) { // Check if keyCode is in keyCodeDict
+      buttonDict[keyCodeDict[keyCode]].click();
+    }
+  });
+
+});
